95e61b4c28aa99aedb0a901f078c2268d48001bf
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-11-04 15:29:39 +0600

    added axios, rechart, react icons, font, primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sora: ["Sora", "serif"],
+      },
+      colors: {
+        primary: "#9538E2",
+      }
+    },
   },
   plugins: [],
 }
